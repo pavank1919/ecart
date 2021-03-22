bb1a299d28f9855b120f1cbc241fc5834a50ad3e
Pavan <pavank1919@gmail.com>
2021-03-22 22:01:08 +0530

    Product component created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Ecart</a>
-      </header>
-      <main>
-        product list
-      </main>
-      <footer>
-        All rights reserved!
-      </footer>
-    </div>
-  );
+import React, { Component } from "react";
+import Products from "./components/Products";
+import data from "./data.json";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">Ecart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart</div>
+          </div>
+        </main>
+        <footer>All rights reserved!</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
